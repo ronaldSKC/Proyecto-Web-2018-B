6dcd099e1f55c7c29388b08096bd72a95881b253
Ronald <ronald_96c@hotmail.com>
2019-01-29 19:04:34 -0500

    pull del miguel

--- Proyecto/backend-mascota/src/adopcion/adopcion.controller.ts
+++ Proyecto/backend-mascota/src/adopcion/adopcion.controller.ts
@@ -1,11 +1,21 @@
-import { Controller, Get, Res } from "@nestjs/common";
+import { Controller, Res, Get } from "@nestjs/common";
 import { AdopcionService } from "./adopcion.service";
 
 @Controller('adopcion')
 export class AdopcionController{
-    constructor(private readonly _adopcionservice:AdopcionService){}
+    
+    constructor(
+        private readonly _adopcionservice:AdopcionService
+        ){  }
+
+    @Get('solicitud')
+    solicitudAdopcion(
+        @Res() response
+    ){
+        response.render('adoptarSolicitud')
 }
 
+}
 export interface Adopcion{
     idSolicitud : number;
     telefono : string;
